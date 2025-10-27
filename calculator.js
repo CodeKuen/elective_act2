9a54eb959d4fdff9d7f0640532c87541233b4ac6
CodeKuen <ulhriclim@gmail.com>
2025-10-27 20:16:41 +0800

    Subtract Commit

--- calculator.js
+++ calculator.js
@@ -1,8 +1,12 @@
-function add(x, y) {
-    return x + y;
-}
-
 let x = 9;
 let y = 11;
 
+function add(x, y) {
+    return x + y;
+}
 console.log("The sum of " + x + " and " + y + " is: " + add(x, y));
+
+function subtract(x, y) {
+    return x - y;
+}
+console.log("The difference of " + x + " and " + y + " is: " + subtract(x, y));
